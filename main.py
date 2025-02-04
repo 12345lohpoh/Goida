e37a4a015de16b8b3d7e8a6e4af05da4078d3210
dencik <davawlol@gmail.com>
2025-02-04 18:58:04 +0300

    задача

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def my_fun(**kwargs):
-    
 
-    print(kwargs)
-    return 0
-
-my_fun(name="test")
+def my_fun():
+    if a%2==0:
+        return True
+    else:
+        return False
+a=int(input())
+print(my_fun())

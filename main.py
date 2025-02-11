03ffa61ac8e4935549549e652f8c81ba85a0e2ff
dencik <davawlol@gmail.com>
2025-02-11 18:43:50 +0300

    .

--- main.py
+++
@@ -1,8 +0,0 @@
-
-def my_fun():
-    if a%2==0:
-        return True
-    else:
-        return False
-a=int(input())
-print(my_fun())
--- задача.py
+++
@@ -1,9 +0,0 @@
-
-def Fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return Fib(n-1)+Fib(n-2)
-print(Fib(5))

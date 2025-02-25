cb2e7ccd47f1ee8038120289991e3c506f0ab1ef
dencik <davawlol@gmail.com>
2025-02-25 18:23:13 +0300

    botKomands

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.reply_to(message,"абаюдненько")
-@bot.message_handler(commands=['skinyArt'])
+@bot.message_handler(commands=['ahab'])
 def start_messageee(message):
-    bot.reply_to(message,"вообще тут стоит функция в которую можно многое засунуть, всякие формулы и разные штуки")
-@bot.message_handler(content_types=['kall'])
+    bot.reply_to(message,"baha")
+@bot.message_handler(content_types='text')
 def start_messagee(message):
-    bot.reply_to(message,"и сразу пуки каки")
+    if message.text == "хззз"
 bot.infinity_polling()
